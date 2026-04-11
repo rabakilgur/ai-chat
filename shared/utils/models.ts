1ae0a8265ca081eb719c8b9a1dbb5c26fb828a48
Robin Garbe <mail@robin-garbe.de>
2026-04-11 22:46:53 +0200

    chore: add additional models

--- shared/utils/models.ts
+++ shared/utils/models.ts
@@ -1,5 +1,20 @@
 export const MODELS = [
-  { label: 'Claude Haiku 4.5', value: 'anthropic/claude-haiku-4.5', icon: 'i-simple-icons-anthropic' },
-  { label: 'Gemini 3 Flash', value: 'google/gemini-3-flash', icon: 'i-simple-icons-google' },
-  { label: 'GPT-5 Nano', value: 'openai/gpt-5-nano', icon: 'i-simple-icons-openai' }
-]
+  {
+    label: "Claude Haiku 4.5",
+    value: "anthropic/claude-haiku-4.5",
+    icon: "i-simple-icons-anthropic",
+  },
+  {
+    label: "Claude Sonnet 4.6",
+    value: "anthropic/claude-sonnet-4.6",
+    icon: "i-simple-icons-anthropic",
+  },
+  {
+    label: "Claude Opus 4.6",
+    value: "anthropic/claude-opus-4.6",
+    icon: "i-simple-icons-anthropic",
+  },
+  { label: "Gemini 3 Flash", value: "google/gemini-3-flash", icon: "i-simple-icons-google" },
+  { label: "GPT-5 Nano", value: "openai/gpt-5-nano", icon: "i-simple-icons-openai" },
+  { label: "GPT-5.4 Pro", value: "openai/gpt-5.4-pro", icon: "i-simple-icons-openai" },
+];
